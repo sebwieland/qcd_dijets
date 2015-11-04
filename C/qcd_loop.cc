30d7e7fb83fec9bdaa87a4e9e901835e1ea29c98
sebwieland <wielandseb@googlemail.com>
2015-11-04 09:42:31 +0100

    include XS Weights

--- C/qcd_loop.cc
+++ C/qcd_loop.cc
@@ -28,27 +28,30 @@ void qcd_dijet(){
   // open files
 TChain* tree_qcd = new TChain("MVATree");
 
-// tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/DiJets_QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_v2_1.root");
-// tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/DiJets_QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_v2_2-pre.root");
+// tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/DiJets_QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_v3_1.root");
+// tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/DiJets_QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_v3_2-pre.root");
 
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_10to15_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_15to30_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_170to300_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_1800to2400_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_2400to3200_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_30to50_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_50to80_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_5to10_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8.root");
-tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v2/QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8.root");
-// tree_qcd-> SetBranchStatus("*",0);
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_10to15_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_15to30_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_170to300_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_1800to2400_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_2400to3200_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_30to50_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_50to80_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_5to10_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8_v3.root");
+tree_qcd->AddFile("/nfs/dust/cms/user/matsch/ttHNtuples/Spring15/v3/QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8_v3.root");
+// tree_qcd-> SetBranchStatus("*",0); 
 
 // initialize variables from tree
+
+
 
 int N_Jets;
 tree_qcd->SetBranchAddress("N_Jets",&N_Jets);
@@ -72,29 +75,33 @@ float* Jet_SumSVMass = new float[120];
 tree_qcd->SetBranchAddress("Jet_SumSVMass",Jet_SumSVMass);
 float* Jet_NSV = new float[120];
 tree_qcd->SetBranchAddress("Jet_NSV",Jet_NSV);
+float Weight_XS;
+tree_qcd->SetBranchAddress("Weight_XS",&Weight_XS);
+tree_qcd->SetWeight(Weight_XS);
 
 
 // define histos  
 TH1F* h_dphi=new TH1F("h_dphi","h_dphi",45,0,3.5);
-TH1F* h_pt3=new TH1F("h_pt3","h_pt3",175,0,3500);
+TH1F* h_pt3=new TH1F("h_pt3","h_pt3",80,0,220);
 TH1F* h_pt3overavg=new TH1F("h_pt3overavg","h_pt3overavg",30,0,2);
 
 TH1F* h_flav_probeJet=new TH1F("h_flav_probeJet","h_flav_probeJet",50,0,23);
 TH1F* h_partonFlav=new TH1F("h_partonFlav","h_partonFlav",50,0,23);
 
-TH1F* h_ptavg=new TH1F("h_ptavg","h_ptavg",175,0,3500);
+TH1F* h_ptavg=new TH1F("h_ptavg","h_ptavg",80,0,220);
+TH1F* h_ptavg_data=new TH1F("h_ptavg_data","h_ptavg_data",80,0,220);
 
-TH1F* h_pthardestjet_lf=new TH1F("h_pthardestjet_lf","h_pthardestjet_lf",175,0,3500);
-TH1F* h_pthardestjet_b=new TH1F("h_pthardestjet_b","h_pthardestjet_b",175,0,3500);
-TH1F* h_pthardestjet_c=new TH1F("h_pthardestjet_c","h_pthardestjet_c",175,0,3500);
+TH1F* h_pthardestjet_lf=new TH1F("h_pthardestjet_lf","h_pthardestjet_lf",80,0,220);
+TH1F* h_pthardestjet_b=new TH1F("h_pthardestjet_b","h_pthardestjet_b",80,0,220);
+TH1F* h_pthardestjet_c=new TH1F("h_pthardestjet_c","h_pthardestjet_c",80,0,220);
 
-TH1F* h_pt_lf=new TH1F("h_pt_lf","h_pt_lf",175,0,3500);
-TH1F* h_pt_b=new TH1F("h_pt_b","h_pt_b",175,0,3500);
-TH1F* h_pt_c=new TH1F("h_pt_c","h_pt_c",175,0,3500);
+TH1F* h_pt_lf=new TH1F("h_pt_lf","h_pt_lf",80,0,220);
+TH1F* h_pt_b=new TH1F("h_pt_b","h_pt_b",80,0,220);
+TH1F* h_pt_c=new TH1F("h_pt_c","h_pt_c",80,0,220);
 
-TH1F* h_pt2_lf=new TH1F("h_pt2_lf","h_pt_lf",175,0,3500);
-TH1F* h_pt2_b=new TH1F("h_pt2_b","h_pt_b",175,0,3500);
-TH1F* h_pt2_c=new TH1F("h_pt2_c","h_pt_c",175,0,3500);
+TH1F* h_pt2_lf=new TH1F("h_pt2_lf","h_pt_lf",80,0,220);
+TH1F* h_pt2_b=new TH1F("h_pt2_b","h_pt_b",80,0,220);
+TH1F* h_pt2_c=new TH1F("h_pt2_c","h_pt_c",80,0,220);
 
 TH1F* h_NJets_lf=new TH1F("h_NJets_lf","h_NJets_lf",16,-0.5,15.5);
 TH1F* h_NJets_b=new TH1F("h_NJets_b","h_NJets_b",16,-0.5,15.5);
@@ -139,7 +146,7 @@ float pt_avg;
 float pt3cut;
 long nentries;
 float SVMassCut;
-float dphicut=2.7;
+float dphicut=3;
 SVMassCut=2;
 pt3cut=0.1;
 // loop over all events
@@ -148,114 +155,118 @@ cout << "total number of MC events: " << nentries << endl;
 for (long iEntry=0;iEntry<nentries;iEntry++) {
 
   if(iEntry%100000==0) cout << "analyzing event " << iEntry << endl;
-//   if(iEntry>2000000) break;
+//   if(iEntry>1000000) break;
   tree_qcd->GetEntry(iEntry);
   if(N_Jets >=2){
     dphi=DeltaPhi;
     h_dphi->Fill(dphi);
     if(N_Jets>=3){
-      h_pt3->Fill(Jet_Pt[2]);
-      h_pt3overavg->Fill(Jet_Pt[2]/PtAve);
+      h_pt3->Fill(Jet_Pt[2],Weight_XS);
+      h_pt3overavg->Fill(Jet_Pt[2]/PtAve,Weight_XS);
       }  
     for (int j=0;j< N_Jets;j++){
-      h_partonFlav->Fill(Jet_PartFlav[j]);
+      h_partonFlav->Fill(Jet_PartFlav[j],Weight_XS);
       if (abs(Jet_PartFlav[j])==4){  
-	h_NJets_c->Fill(N_Jets);
-	h_CSV_c->Fill(Jet_CSV[j]);
+	h_NJets_c->Fill(N_Jets,Weight_XS);
+	h_CSV_c->Fill(Jet_CSV[j],Weight_XS);
 	}
 	    
       else if (abs(Jet_PartFlav[j])==5){ 
-	h_NJets_b->Fill(N_Jets);
-	h_CSV_b->Fill(Jet_CSV[j]);
+	h_NJets_b->Fill(N_Jets,Weight_XS);
+	h_CSV_b->Fill(Jet_CSV[j],Weight_XS);
 	}
 	    
       else if (abs(Jet_PartFlav[j])!=5 && abs(Jet_PartFlav[j])!=4){
-	h_NJets_lf->Fill(N_Jets);
-	h_CSV_lf->Fill(Jet_CSV[j]);
+	h_NJets_lf->Fill(N_Jets,Weight_XS);
+	h_CSV_lf->Fill(Jet_CSV[j],Weight_XS);
 	}
       }
       
     if (dphi> dphicut ){	
       if (abs(Jet_Eta[0])<2.1 && abs(Jet_Eta[1])<2.1){
 	pt_avg=PtAve;
-	h_ptavg->Fill(pt_avg);
+	h_ptavg->Fill(pt_avg,Weight_XS);
 	  
 	if (abs(Jet_PartFlav[0])==4){
-	  h_pthardestjet_c->Fill(Jet_Pt[0]);
-	  h_pt2_c->Fill(Jet_Pt[1]);
+	  h_pthardestjet_c->Fill(Jet_Pt[0],Weight_XS);
+	  h_pt2_c->Fill(Jet_Pt[1],Weight_XS);
 	}	    
 	else if (abs(Jet_PartFlav[0])==5){ 
-	  h_pthardestjet_b->Fill(Jet_Pt[0]);
-	  h_pt2_b->Fill(Jet_Pt[1]);
+	  h_pthardestjet_b->Fill(Jet_Pt[0],Weight_XS);
+	  h_pt2_b->Fill(Jet_Pt[1],Weight_XS);
 	}	  
 	else if ( abs(Jet_PartFlav[0])!=5 && abs(Jet_PartFlav[0])!=4){
-	  h_pthardestjet_lf->Fill(Jet_Pt[0]);
-	  h_pt2_lf->Fill(Jet_Pt[1]);
+	  h_pthardestjet_lf->Fill(Jet_Pt[0],Weight_XS);
+	  h_pt2_lf->Fill(Jet_Pt[1],Weight_XS);
 	}
       
 	for (int j=0;j<2;j++){	
 	  if (abs(Jet_PartFlav[j])==5 && abs(Jet_HadFlav[j]==5)) {
-	    h_SumSVMass_part5_had5->Fill(Jet_SumSVMass[j]);
-	    h_NSV_part5_had5->Fill(Jet_NSV[j]);
+	    h_SumSVMass_part5_had5->Fill(Jet_SumSVMass[j],Weight_XS);
+	    h_NSV_part5_had5->Fill(Jet_NSV[j],Weight_XS);
 	  }
 	  if (abs(Jet_PartFlav[j])==21 && abs(Jet_HadFlav[j]==5)){
-	    h_SumSVMass_part21_had5->Fill(Jet_SumSVMass[j]);
-	    h_NSV_part21_had5->Fill(Jet_NSV[j]);
+	    h_SumSVMass_part21_had5->Fill(Jet_SumSVMass[j],Weight_XS);
+	    h_NSV_part21_had5->Fill(Jet_NSV[j],Weight_XS);
 	  }
-	  if (abs(Jet_PartFlav[j])==4) h_pt_c->Fill(Jet_Pt[j]);
-	  else if (abs(Jet_PartFlav[j])==5) h_pt_b->Fill(Jet_Pt[j]);
-	  else if (abs(Jet_PartFlav[j])!=5 && abs(Jet_PartFlav[j])!=4) h_pt_lf->Fill(Jet_Pt[j]);
+	  if (abs(Jet_PartFlav[j])==4) h_pt_c->Fill(Jet_Pt[j],Weight_XS);
+	  else if (abs(Jet_PartFlav[j])==5) h_pt_b->Fill(Jet_Pt[j],Weight_XS);
+	  else if (abs(Jet_PartFlav[j])!=5 && abs(Jet_PartFlav[j])!=4) h_pt_lf->Fill(Jet_Pt[j],Weight_XS);
 	  //csv tagging
 	  if (Jet_CSV[j]>0.97){
 	    if (N_Jets >=3){
 		  if (Jet_Pt[2]/pt_avg < pt3cut){
 		      if (j==0){
-			h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[0]));
+			h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[0]),Weight_XS);
+			h_flav_probeJet->Fill(abs(Jet_PartFlav[1]),Weight_XS);
 		      }
 		      else if (j==1){
-			h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[1]));
+			h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[1]),Weight_XS);
+			h_flav_probeJet->Fill(abs(Jet_PartFlav[0]),Weight_XS);
 		      }
 		}
 	    }
 	    else if (j==0 && N_Jets ==2){
-		  h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[0]));
+		  h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[0]),Weight_XS);
+		  h_flav_probeJet->Fill(abs(Jet_PartFlav[1]),Weight_XS);
 		}
 	    else if (j==1 && N_Jets ==2){
-		  h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[1]));
+		  h_tagjet_partflav_wo_SVMassCut->Fill(abs(Jet_PartFlav[1]),Weight_XS);
+		  h_flav_probeJet->Fill(abs(Jet_PartFlav[0]),Weight_XS);
 		}
 		
 	    if (Jet_SumSVMass[j]<SVMassCut && Jet_NSV[j]==1){
 		if (N_Jets >=3){
 		  if (Jet_Pt[2]/pt_avg < pt3cut){
 		      if (j==0){
-			h_tagjet_hadflav->Fill(abs(Jet_HadFlav[0]));
-			h_tagjet_partflav->Fill(abs(Jet_PartFlav[0]));
-			if (abs(Jet_PartFlav[1])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[1]);
-			else if (abs(Jet_PartFlav[1])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[1]);
-			else if (abs(Jet_PartFlav[1])!=4 && abs(Jet_PartFlav[1])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[1]);
+			h_tagjet_hadflav->Fill(abs(Jet_HadFlav[0]),Weight_XS);
+			h_tagjet_partflav->Fill(abs(Jet_PartFlav[0]),Weight_XS);
+			if (abs(Jet_PartFlav[1])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[1],Weight_XS);
+			else if (abs(Jet_PartFlav[1])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[1],Weight_XS);
+			else if (abs(Jet_PartFlav[1])!=4 && abs(Jet_PartFlav[1])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[1],Weight_XS);
 		      }
 		      else if (j==1){
-			h_tagjet_hadflav->Fill(abs(Jet_HadFlav[1]));
-			h_tagjet_partflav->Fill(abs(Jet_PartFlav[1]));
-			if (abs(Jet_PartFlav[0])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[0]);
-			else if (abs(Jet_PartFlav[0])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[0]);
-			else if (abs(Jet_PartFlav[0])!=4 && abs(Jet_PartFlav[0])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[0]);
+			h_tagjet_hadflav->Fill(abs(Jet_HadFlav[1]),Weight_XS);
+			h_tagjet_partflav->Fill(abs(Jet_PartFlav[1]),Weight_XS);
+			if (abs(Jet_PartFlav[0])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[0],Weight_XS);
+			else if (abs(Jet_PartFlav[0])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[0],Weight_XS);
+			else if (abs(Jet_PartFlav[0])!=4 && abs(Jet_PartFlav[0])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[0],Weight_XS);
 		      }
 		}
 		}
 		else if (j==0 && N_Jets ==2){
-		  h_tagjet_hadflav->Fill(abs(Jet_HadFlav[0]));
-		  h_tagjet_partflav->Fill(abs(Jet_PartFlav[0]));
-		  if (abs(Jet_PartFlav[1])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[1]);
-		  else if (abs(Jet_PartFlav[1])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[1]);
-		  else if (abs(Jet_PartFlav[1])!=4 && abs(Jet_PartFlav[1])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[1]);
+		  h_tagjet_hadflav->Fill(abs(Jet_HadFlav[0]),Weight_XS);
+		  h_tagjet_partflav->Fill(abs(Jet_PartFlav[0]),Weight_XS);
+		  if (abs(Jet_PartFlav[1])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[1],Weight_XS);
+		  else if (abs(Jet_PartFlav[1])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[1],Weight_XS);
+		  else if (abs(Jet_PartFlav[1])!=4 && abs(Jet_PartFlav[1])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[1],Weight_XS);
 		}
 		else if (j==1 && N_Jets ==2){
-		  h_tagjet_hadflav->Fill(abs(Jet_HadFlav[1]));
-		  h_tagjet_partflav->Fill(abs(Jet_PartFlav[1]));
-		  if (abs(Jet_PartFlav[0])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[0]);
-		  else if (abs(Jet_PartFlav[0])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[0]);
-		  else if (abs(Jet_PartFlav[0])!=4 && abs(Jet_PartFlav[0])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[0]);
+		  h_tagjet_hadflav->Fill(abs(Jet_HadFlav[1]),Weight_XS);
+		  h_tagjet_partflav->Fill(abs(Jet_PartFlav[1]),Weight_XS);
+		  if (abs(Jet_PartFlav[0])==4) h_CSV_taggedhigh_c->Fill(Jet_CSV[0],Weight_XS);
+		  else if (abs(Jet_PartFlav[0])==5) h_CSV_taggedhigh_b->Fill(Jet_CSV[0],Weight_XS);
+		  else if (abs(Jet_PartFlav[0])!=4 && abs(Jet_PartFlav[0])!=5) h_CSV_taggedhigh_lf->Fill(Jet_CSV[0],Weight_XS);
 		}	  
 	    }
 	  }
@@ -264,26 +275,26 @@ for (long iEntry=0;iEntry<nentries;iEntry++) {
 		if (N_Jets >=3){
 		  if (Jet_Pt[2]/pt_avg < pt3cut){
 		      if (j==0){
-			if (abs(Jet_HadFlav[1])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[1]);
-			else if (abs(Jet_HadFlav[1])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[1]);
-			else if (abs(Jet_HadFlav[1])!=4 && abs(Jet_HadFlav[1])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[1]);
+			if (abs(Jet_HadFlav[1])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[1],Weight_XS);
+			else if (abs(Jet_HadFlav[1])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[1],Weight_XS);
+			else if (abs(Jet_HadFlav[1])!=4 && abs(Jet_HadFlav[1])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[1],Weight_XS);
 		      }
 		      else if (j==1){
-			if (abs(Jet_HadFlav[0])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[0]);
-			else if (abs(Jet_HadFlav[0])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[0]);
-			else if (abs(Jet_HadFlav[0])!=4 && abs(Jet_HadFlav[0])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[0]);
+			if (abs(Jet_HadFlav[0])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[0],Weight_XS);
+			else if (abs(Jet_HadFlav[0])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[0],Weight_XS);
+			else if (abs(Jet_HadFlav[0])!=4 && abs(Jet_HadFlav[0])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[0],Weight_XS);
 		      }
 		  }
 		}
 		if (j==0 && N_Jets ==2 ){
-		  if (abs(Jet_HadFlav[1])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[1]);
-		  else if (abs(Jet_HadFlav[1])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[1]);
-		  else if (abs(Jet_HadFlav[1])!=4 && abs(Jet_PartFlav[1])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[1]);
+		  if (abs(Jet_HadFlav[1])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[1],Weight_XS);
+		  else if (abs(Jet_HadFlav[1])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[1],Weight_XS);
+		  else if (abs(Jet_HadFlav[1])!=4 && abs(Jet_PartFlav[1])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[1],Weight_XS);
 		}
 		else if (j==1 && N_Jets ==2){
-		  if (abs(Jet_HadFlav[0])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[0]);
-		  else if (abs(Jet_HadFlav[0])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[0]);
-		  else if (abs(Jet_HadFlav[0])!=4 && abs(Jet_PartFlav[0])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[0]);
+		  if (abs(Jet_HadFlav[0])==4) h_CSV_taggedlow_c->Fill(Jet_CSV[0],Weight_XS);
+		  else if (abs(Jet_HadFlav[0])==5) h_CSV_taggedlow_b->Fill(Jet_CSV[0],Weight_XS);
+		  else if (abs(Jet_HadFlav[0])!=4 && abs(Jet_PartFlav[0])!=5) h_CSV_taggedlow_lf->Fill(Jet_CSV[0],Weight_XS);
 		}
 	  }
 	  //true tagging
@@ -672,11 +683,12 @@ float nentries_data = tree_data->GetEntries();
 cout << "total number of data events: " << nentries_data << endl;
 for (long iEntry=0;iEntry<nentries_data;iEntry++) {
   if(iEntry%100000==0) cout << "analyzing event " << iEntry << endl;
-//   if(iEntry>2000000) break;
+//   if(iEntry>100000) break;
   tree_data->GetEntry(iEntry);
   if (dphidata > dphicut){
     if (N_Jets_data>=2){
       if (abs(Jet_Eta_data[0])<2.1 && abs(Jet_Eta_data[1])<2.1){
+	h_ptavg_data->Fill(pt_avg_data);
 	for(int j=0;j<2;j++){      
 	      //LF tagging
 	  if (Jet_CSV_data[j]<0.605 ){      
@@ -701,7 +713,9 @@ for (long iEntry=0;iEntry<nentries_data;iEntry++) {
 
 
 h_CSV_data_lf->Write();
-
+h_ptavg_data->Write();
+h_ptavg_data->Draw();
+c1->SaveAs("ptave_data.png");
 // plot and define CSV ratio plot
 float dataevents_lf=h_CSV_data_lf->Integral();
 cout << dataevents_lf  << " events in data  selected" << endl;
@@ -753,7 +767,7 @@ TH1F* mc_norm=(TH1F*)mc_stack_lf_normalized->GetStack()->Last();
 cout << "mcevents after normalization: " << mc_norm->Integral() << endl;
 
 //draw histos
-// h_CSV_data_lf->Sumw2();
+h_CSV_data_lf->Sumw2();
 mc_stack_lf_normalized->Draw("hist");
 h_CSV_data_lf->SetMarkerStyle(20);
 h_CSV_data_lf->Draw("SAMEE0");
@@ -800,7 +814,7 @@ diff_lf->Draw("E0");
 diff_lf->SetLineColor(kBlue);
 diff_lf->Write();
 
-TH1F* nonb_lf=(TH1F*)h_CSV_data_lf->Clone();
+TH1F* nonb_lf=(TH1F*)h_CSV_taggedlow_lf->Clone();
 nonb_lf->Add(h_CSV_taggedlow_c);
 nonb_lf->Draw("SAMEE0");
 nonb_lf->SetMarkerStyle(20);
